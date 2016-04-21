b386da0a2e57bc1d5f2ccfb60daa912f2f832ed2
smithcts <smithcts93@gmail.com>
2016-04-20 19:11:05 -0500

    Added tabs and functionality to multipage gui, remains untested

--- Senior_Design_Eclipse_Workspace/src/Multi_Page.cpp
+++ Senior_Design_Eclipse_Workspace/src/Multi_Page.cpp
@@ -71,52 +71,49 @@ extern Encoder encoder;
 *  It has been created manually, but could also be created by the GUIBuilder.
 */
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate1[] = {
-  { WINDOW_CreateIndirect, "Motor Control", 0,         		0,  0, 480, 272, 0},
+{ WINDOW_CreateIndirect, "Motor Control", 0,         			0,  0, 480, 272, 0},
   { TEXT_CreateIndirect,     "Azimuthal:" ,  0,          		5,  35,  60,  20, TEXT_CF_LEFT },
   { TEXT_CreateIndirect,     "Vertical:", 0,             		5,  85,  60,  20, TEXT_CF_LEFT },
   { TEXT_CreateIndirect,     "Claw:",  0,                		5,  135, 60,  20, TEXT_CF_LEFT },
-  { BUTTON_CreateIndirect,   "Azimuthal",   GUI_ID_BUTTON0,   	110, 160,  80,  40 },
-  { BUTTON_CreateIndirect,   "Vertical",    GUI_ID_BUTTON1,    	200, 160,  80,  40 },
-  { BUTTON_CreateIndirect,   "Claw",     	GUI_ID_BUTTON2,     290, 160,  80,  40 },
+  { BUTTON_CreateIndirect,   "Azimuthal",   	GUI_ID_BUTTON0,   	110, 160,  80,  40 },
+  { BUTTON_CreateIndirect,   "Vertical",    	GUI_ID_BUTTON1,    	200, 160,  80,  40 },
+  { BUTTON_CreateIndirect,   "Claw",     	GUI_ID_BUTTON2,     	290, 160,  80,  40 },
   { BUTTON_CreateIndirect,   "Stop", 		GUI_ID_BUTTON3,  	380, 160,  80,  40 },
-  { BUTTON_CreateIndirect,   "Forward",     GUI_ID_BUTTON4,      10,  70,  80,  30 },
-  { BUTTON_CreateIndirect,   "Reverse",     GUI_ID_BUTTON5,      10, 110,  80,  30 },
+  { BUTTON_CreateIndirect,   "Forward",     	GUI_ID_BUTTON4,      	 10,  70,  80,  30 },
+  { BUTTON_CreateIndirect,   "Reverse",     	GUI_ID_BUTTON5,      	 10, 110,  80,  30 },
 };
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate2[] = {
-  { WINDOW_CreateIndirect,    "Dialog 2", 0,                   0,   0, 260, 100, FRAMEWIN_CF_MOVEABLE },
-  { CHECKBOX_CreateIndirect,  "",         GUI_ID_CHECK0,       5,  30,   0,   0, 0},
-  { CHECKBOX_CreateIndirect,  "",         GUI_ID_CHECK1,       5,  50,   0,   0, 0},
-  { TEXT_CreateIndirect,      "Select 0", GUI_ID_TEXT0,       25,  30,  50,  15, TEXT_CF_LEFT },
-  { TEXT_CreateIndirect,      "Select 1", GUI_ID_TEXT1,       25,  50,  50,  15, TEXT_CF_LEFT },
-  { TEXT_CreateIndirect,      "Dialog 2", 0,                   5,  10,  50,  20, TEXT_CF_LEFT }
+  { WINDOW_CreateIndirect,    "Dialog 2", 0,                   			0,   0, 480, 272, 0 },
+  { BUTTON_CreateIndirect,   "ClockWise",   		GUI_ID_BUTTON0,   	100,  50,  80,  40 },
+  { BUTTON_CreateIndirect,   "Counter-Clockwise",    	GUI_ID_BUTTON1,    	200, 100,  80,  40 },
+  { BUTTON_CreateIndirect,   "Stop",     		GUI_ID_BUTTON2,     	300, 150,  80,  40 },
 };
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate3[] = {
-  { WINDOW_CreateIndirect,    "Dialog 3", 0,                   0,   0, 260, 100, FRAMEWIN_CF_MOVEABLE },
-  { RADIO_CreateIndirect,     "",         GUI_ID_RADIO0,       5,  30,   0,   0, 0, 3},
-  { TEXT_CreateIndirect,      "Option 1", GUI_ID_TEXT0,       25,  30,  50,  15, TEXT_CF_LEFT },
-  { TEXT_CreateIndirect,      "Option 2", GUI_ID_TEXT1,       25,  50,  50,  15, TEXT_CF_LEFT },
-  { TEXT_CreateIndirect,      "Option 3", GUI_ID_TEXT2,       25,  70,  50,  15, TEXT_CF_LEFT },
-  { TEXT_CreateIndirect,      "Dialog 3", 0,                   5,  10,  50,  20, TEXT_CF_LEFT }
+  { WINDOW_CreateIndirect,    "Dialog 3", 0,                   		0,   0, 480, 272, 0 },
+  { BUTTON_CreateIndirect,   "Up",   		GUI_ID_BUTTON0,   	100,  50,  80,  40 },
+  { BUTTON_CreateIndirect,   "Down",    	GUI_ID_BUTTON1,    	200, 100,  80,  40 },
+  { BUTTON_CreateIndirect,   "Stop",     	GUI_ID_BUTTON2,     	300, 150,  80,  40 },
 };
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate4[] = {
-  { WINDOW_CreateIndirect,    "Dialog 4", 0,                   0,   0, 260, 100, FRAMEWIN_CF_MOVEABLE },
-  { MULTIEDIT_CreateIndirect, "Text",     GUI_ID_MULTIEDIT0,   5,  30, 200,  40  },
-  { TEXT_CreateIndirect,      "Dialog 4", 0,                   5,  10,  50,  20, TEXT_CF_LEFT }
+  { WINDOW_CreateIndirect,   "Dialog 4", 0,                       	0,   0, 480, 272, 0 },
+  { BUTTON_CreateIndirect,   "Open",   		GUI_ID_BUTTON0,   	100,  50,  80,  40 },
+  { BUTTON_CreateIndirect,   "Close",    	GUI_ID_BUTTON1,    	200, 100,  80,  40 },
+  { BUTTON_CreateIndirect,   "Stop",     	GUI_ID_BUTTON2,     	300, 150,  80,  40 },
 };
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate5[] = {
-  { WINDOW_CreateIndirect,    "Dialog 5", 0,                   0,   0, 260, 100, FRAMEWIN_CF_MOVEABLE },
+  { WINDOW_CreateIndirect,    "Dialog 5", 0,                   0,   0, 480, 272, 0 },
   { SLIDER_CreateIndirect,    "",         GUI_ID_SLIDER0,      5,  30, 200,  40  },
-  { TEXT_CreateIndirect,      "Dialog 5", 0,                   5,  10,  50,  20, TEXT_CF_LEFT }
+  { TEXT_CreateIndirect,      "Dialog 5", 0,                   5,  10,  50,  20, TEXT_CF_LEFT },
 };
 
 static const GUI_WIDGET_CREATE_INFO _aDialogCreate6[] = {
-  { WINDOW_CreateIndirect,    "Dialog 6", 0,                   0,   0, 260, 100, FRAMEWIN_CF_MOVEABLE },
+  { WINDOW_CreateIndirect,    "Dialog 6", 0,                   0,   0, 480, 272, 0 },
   { SCROLLBAR_CreateIndirect, "",         GUI_ID_SCROLLBAR0,   5,  30, 200,  40  },
-  { TEXT_CreateIndirect,      "Dialog 6", 0,                   5,  10,  50,  20, TEXT_CF_LEFT }
+  { TEXT_CreateIndirect,      "Dialog 6", 0,                   5,  10,  50,  20, TEXT_CF_LEFT },
 };
 
 /*********************************************************************
@@ -213,18 +210,154 @@ static void _cbDialog1(WM_MESSAGE * pMsg) {
 }
 /*********************************************************************
 *
+*       _cbDialog2
+*/
+static void _cbDialog2(WM_MESSAGE * pMsg) {
+  WM_HWIN hWinSrc;
+  int Id, NCode;
+  hWinSrc = pMsg->hWinSrc;
+  switch (pMsg->MsgId) {
+  case WM_NOTIFY_PARENT:
+    Id    = WM_GetId(hWinSrc);                    /* Id of widget */
+    NCode = pMsg->Data.v;                         /* Notification code */
+    switch (NCode){
+    case WM_NOTIFICATION_RELEASED:
+        if (Id == GUI_ID_BUTTON0) {       		// Up Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_SET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 0) EncoderEnable[i] = true;
+
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(AzimuthalCount);
+      	  VerticalMotor.dutyCycle(0);	// Ensures the other motors are off
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON1) {        		// Down Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_RESET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 1) EncoderEnable[i] = true;
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(AzimuthalCount);
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON2) {        		// Stop Button
+      	  AzimuthalMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        break;
+    }
+    break;
+  default:
+    WM_DefaultProc(pMsg);
+  }
+}
+
+/*********************************************************************
+*
+*       _cbDialog3
+*/
+static void _cbDialog3(WM_MESSAGE * pMsg) {
+  WM_HWIN hWinSrc;
+  int Id, NCode;
+  hWinSrc = pMsg->hWinSrc;
+  switch (pMsg->MsgId) {
+  case WM_NOTIFY_PARENT:
+    Id    = WM_GetId(hWinSrc);                    /* Id of widget */
+    NCode = pMsg->Data.v;                         /* Notification code */
+    switch (NCode){
+    case WM_NOTIFICATION_RELEASED:
+        if (Id == GUI_ID_BUTTON0) {       		// Open Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_SET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 0) EncoderEnable[i] = true;
+
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(VerticalCount);
+      	  VerticalMotor.dutyCycle(0);	// Ensures the other motors are off
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON1) {        		// Close Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_RESET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 1) EncoderEnable[i] = true;
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(VerticalCount);
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON2) {        		// Stop Button
+      	  AzimuthalMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        break;
+    }
+    break;
+  default:
+    WM_DefaultProc(pMsg);
+  }
+}
+
+/*********************************************************************
+*
 *       _cbDialog4
 */
 static void _cbDialog4(WM_MESSAGE * pMsg) {
-  WM_HWIN hItem;
-  WM_HWIN hDlg;
-
-  hDlg = pMsg->hWin;
+  WM_HWIN hWinSrc;
+  int Id, NCode;
+  hWinSrc = pMsg->hWinSrc;
   switch (pMsg->MsgId) {
-  case WM_INIT_DIALOG:
-    hItem = WM_GetDialogItem(hDlg, GUI_ID_MULTIEDIT0);
-    MULTIEDIT_SetText(hItem, "MULTIEDIT widget");
-    MULTIEDIT_SetInsertMode(hItem, 1);
+  case WM_NOTIFY_PARENT:
+    Id    = WM_GetId(hWinSrc);                    /* Id of widget */
+    NCode = pMsg->Data.v;                         /* Notification code */
+    switch (NCode){
+    case WM_NOTIFICATION_RELEASED:
+        if (Id == GUI_ID_BUTTON0) {       		// Open Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_SET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 0) EncoderEnable[i] = true;
+
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(ClawCount);
+      	  VerticalMotor.dutyCycle(0);	// Ensures the other motors are off
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON1) {        		// Close Button
+          HAL_GPIO_WritePin(GPIOI, GPIO_PIN_3, GPIO_PIN_RESET);
+      	  for (int8_t i = 0; i < 3; i++)
+      	  {
+      		  if (i == 1) EncoderEnable[i] = true;
+      		  else EncoderEnable[i] = false;
+      	  }
+      	  encoder.set(ClawCount);
+      	  AzimuthalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+        }
+        if (Id == GUI_ID_BUTTON2) {        		// Stop Button
+      	  AzimuthalMotor.dutyCycle(0);
+      	  VerticalMotor.dutyCycle(0);
+      	  ClawMotor.dutyCycle(0);
+        }
+        break;
+    }
     break;
   default:
     WM_DefaultProc(pMsg);
@@ -294,26 +427,25 @@ void MainTask(void) {
   // Create and attach the MULTIPAGE dialog windows
   //
   MULTIPAGE_SetTabHeight(hMultiPage, 40);
-  MULTIPAGE_SetTabWidth(hMultiPage, 80, 0);
 
   hDialog = GUI_CreateDialogBox(_aDialogCreate1, GUI_COUNTOF(_aDialogCreate1), _cbDialog1, WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Debug");
-  GUI_Delay(500);
-  hDialog = GUI_CreateDialogBox(_aDialogCreate2, GUI_COUNTOF(_aDialogCreate2), NULL,       WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Page 2");
-  GUI_Delay(500);
-  hDialog = GUI_CreateDialogBox(_aDialogCreate3, GUI_COUNTOF(_aDialogCreate3), NULL,       WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Page 3");
-  GUI_Delay(500);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	Debug	");
+  GUI_Delay(50);
+  hDialog = GUI_CreateDialogBox(_aDialogCreate2, GUI_COUNTOF(_aDialogCreate2), _cbDialog2,       WM_UNATTACHED, 0, 0);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	Azimuthal	");
+  GUI_Delay(50);
+  hDialog = GUI_CreateDialogBox(_aDialogCreate3, GUI_COUNTOF(_aDialogCreate3), _cbDialog3,       WM_UNATTACHED, 0, 0);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	Vertical	");
+  GUI_Delay(50);
   hDialog = GUI_CreateDialogBox(_aDialogCreate4, GUI_COUNTOF(_aDialogCreate4), _cbDialog4, WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Page 4");
-  GUI_Delay(500);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	Claw	");
+  GUI_Delay(50);
   hDialog = GUI_CreateDialogBox(_aDialogCreate5, GUI_COUNTOF(_aDialogCreate5), NULL,       WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Page 5");
-  GUI_Delay(500);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	Page 5	");
+  GUI_Delay(50);
   hDialog = GUI_CreateDialogBox(_aDialogCreate6, GUI_COUNTOF(_aDialogCreate6), NULL,       WM_UNATTACHED, 0, 0);
-  MULTIPAGE_AddPage(hMultiPage, hDialog, "Page 6");
-  GUI_Delay(500);
+  MULTIPAGE_AddPage(hMultiPage, hDialog, "	About	");
+  GUI_Delay(50);
   //
   // Demonstrate the use of MULTIPAGE_SetAlign
   //
