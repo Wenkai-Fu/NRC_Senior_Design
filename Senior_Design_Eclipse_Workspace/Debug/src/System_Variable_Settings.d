d126d472424f450e4f87b9edf7e882c9faae407c
smithcts <smithcts93@gmail.com>
2016-04-29 16:11:59 -0500

    removed the debug folder, its not needed

--- Senior_Design_Eclipse_Workspace/Debug/src/System_Variable_Settings.d
+++
@@ -1,4 +0,0 @@
-src/System_Variable_Settings.o: ../src/System_Variable_Settings.cpp \
- C:/Users/smith/Documents/GitHub/NRC_Senior_Design/Senior_Design_Eclipse_Workspace/inc/System_Variable_Settings.h
-
-C:/Users/smith/Documents/GitHub/NRC_Senior_Design/Senior_Design_Eclipse_Workspace/inc/System_Variable_Settings.h:
--- Senior_Design_Eclipse_Workspace/Debug/src/pid_controller.d
+++
@@ -1,4 +0,0 @@
-src/pid_controller.o: ../src/pid_controller.cpp \
- C:/Users/smith/Documents/GitHub/NRC_Senior_Design/Senior_Design_Eclipse_Workspace/inc/pid_controller.h
-
-C:/Users/smith/Documents/GitHub/NRC_Senior_Design/Senior_Design_Eclipse_Workspace/inc/pid_controller.h:
